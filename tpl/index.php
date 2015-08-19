33d48e4b6df7babb52258f65f60a57627a319ddc
Michael B <developer@mihael.me>
2015-08-19 09:13:05 +0000

    blog template

---
+++ tpl/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css" />
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <title>Blog</title>
+</head>
+<body>
+    <h1 class="main-title">My Blog</h1>
+    <div class="container-fluid">
+        <div class="col-md-3">
+            <ul class="nav">
+                <li>
+                    <a href="#">Main</a>
+                </li>
+                <li>
+                    <a href="#">Create Post</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-9 blog-body">
+            <div class="post">
+                <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                <h3 class="post-subtitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </h3>
+                    
+                <p class="post-meta"><span class="glyphicon glyphicon-time"></span> Posted by <a href="#">Start Bootstrap</a> on September 24, 2014
+                    <a href="#" class="btn btn-primary btn-sm pull-right">Read More</a>
+                </p>
+                
+                <hr />
+            </div>
+            
+            <div class="post">
+                <h2 class="post-title">Failure is not an option</h2>
+                <h3 class="post-subtitle">
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut reru.
+                </h3>
+                    
+                <p class="post-meta"><span class="glyphicon glyphicon-time"></span> Posted by <a href="#">Start Bootstrap</a> on September 24, 2014
+                    <a href="#" class="btn btn-primary btn-sm pull-right">Read More</a>
+                </p>
+                <hr />
+            </div>
+            
+            <div class="post">
+                <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2>
+                <h3 class="post-subtitle">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                </h3>
+                    
+                <p class="post-meta"><span class="glyphicon glyphicon-time"></span> Posted by <a href="#">Start Bootstrap</a> on September 24, 2014
+                    <a href="#" class="btn btn-primary btn-sm pull-right">Read More</a>
+                </p>
+                <hr />
+            </div>
+            
+             <div class="post">
+                <h2 class="post-title">Science has not yet mastered prophecy</h2>
+                <h3 class="post-subtitle">
+                    But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. 
+                </h3>
+                    
+                <p class="post-meta"><span class="glyphicon glyphicon-time"></span> Posted by <a href="#">Start Bootstrap</a> on September 24, 2014
+                    <a href="#" class="btn btn-primary btn-sm pull-right">Read More</a>
+                </p>
+                <hr />
+            </div>
+        </div>    
+        
+    </div>
+</body>
+</html>
